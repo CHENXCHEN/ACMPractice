b88021f1f0615457be95028861a1451a396f3909
CHENXCHEN <chen93104@163.com>
2018-10-28 17:54:01 +0800

    Sock Merchant Solution

--- src/main/scala/hackerrank/Practice/InterviewPreparationKit/WarmupChallenges/SockMerchant/Solution.scala
+++ src/main/scala/hackerrank/Practice/InterviewPreparationKit/WarmupChallenges/SockMerchant/Solution.scala
@@ -7,16 +7,24 @@ import java.util.concurrent._
 import java.util.function._
 import java.util.regex._
 import java.util.stream._
+import scala.collection.mutable
 
 object Solution {
 
   // Complete the sockMerchant function below.
   def sockMerchant(n: Int, ar: Array[Int]): Int = {
-
-
+    val map = mutable.Map[Int, Int]()
+    ar.foreach(
+      x => map += x -> (map.getOrElse(x, 0) + 1)
+    )
+    map.values.map(_ / 2).sum
   }
 
   def main(args: Array[String]) {
+    if (!sys.env.keySet.contains("OUTPUT_PATH")) {
+      testData()
+      return
+    }
     val stdin = scala.io.StdIn
 
     val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
@@ -29,5 +37,10 @@ object Solution {
     printWriter.println(result)
 
     printWriter.close()
+  }
+
+
+  def testData(): Unit = {
+    println(sockMerchant(9, Array(10,20,20,10,10,30,50,10,20)))
   }
 }
