117234cb28bd802ceeb8bebe4de69e1206ea9dc8
CHENXCHEN <chenhuachaoxyz@gmail.com>
2022-06-18 23:31:04 +0800

    leetcode n2475【链表，指针】

---
+++ src/main/java/leetcode/normal/j2475/Solution.java
@@ -0,0 +1,31 @@
+package leetcode.normal.j2475;
+
+/**
+ * Created by CHENXCHEN
+ * https://leetcode.cn/problems/4ueAj6/
+ * 剑指 Offer II 029. 排序的循环链表
+ *
+ * @author <a href="mailto:chenhuachaoxyz@gmail.com">报时</a>
+ */
+class Solution {
+    public Node insert(Node head, int insertVal) {
+        if (head == null) {
+            Node node = new Node(insertVal);
+            node.next = node;
+            return node;
+        }
+        Node root = head;
+        int circleCnt = 0;
+        while (true) {
+            if ((root.val <= insertVal && insertVal < root.next.val)
+                    || (root.val > root.next.val && insertVal >= root.val)
+                    || (root.val > root.next.val && insertVal <= root.next.val)
+                    || (circleCnt == 1 && root.next == head)) {
+                root.next = new Node(insertVal, root.next);
+                break;
+            } else if (root.next == head) circleCnt++;
+            root = root.next;
+        }
+        return head;
+    }
+}
