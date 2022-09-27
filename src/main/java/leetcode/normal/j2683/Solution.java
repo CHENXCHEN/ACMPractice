4f182817d9157392e3ab2c2a9e3ef5d021e1a430
CHENXCHEN <chenhuachaoxyz@gmail.com>
2022-09-27 22:34:07 +0800

    leetcode n2683【哈希】

---
+++ src/main/java/leetcode/normal/j2683/Solution.java
@@ -0,0 +1,22 @@
+package leetcode.normal.j2683;
+
+/**
+ * Created by CHENXCHEN
+ * <a href="https://leetcode.cn/problems/check-permutation-lcci/">面试题 01.02. 判定是否互为字符重排</a>
+ *
+ * @author <a href="mailto:chenhuachaoxyz@gmail.com">报时</a>
+ */
+class Solution {
+    public boolean CheckPermutation(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        int[] has = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            has[s1.charAt(i) - 'a']++;
+            has[s2.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (has[i] != 0) return false;
+        }
+        return true;
+    }
+}
