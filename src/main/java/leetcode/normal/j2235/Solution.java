69ddd86473eea376a64bf864bb5eba3bffdca742
CHENXCHEN <chenhuachaoxyz@gmail.com>
2023-08-19 21:21:32 +0800

    leetcode n2235【模拟】

---
+++ src/main/java/leetcode/normal/j2235/Solution.java
@@ -0,0 +1,13 @@
+package leetcode.normal.j2235;
+
+/**
+ * Created by CHENXCHEN
+ * <a href="https://leetcode.cn/problems/add-two-integers/">2235. 两整数相加</a>
+ *
+ * @author <a href="mailto:chenhuachaoxyz@gmail.com">报时</a>
+ */
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
